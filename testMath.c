b782cbc19a132a756bb67afe312710c904e35586
Tanin Nantakitgosol <tnantaki@z2t3c2.42bangkok.com>
2023-09-02 13:51:18 +0700

    Fix scene

--- testMath.c
+++ testMath.c
@@ -4,7 +4,7 @@ int	main(void)
 {
 	t_cor	vec = {1, 1, 0};
 	t_cor	dir = {2, 1, 0};
-	dir = vec_norm(dir);
+	// dir = vec_norm(dir);
 	float	result = vec_dot(vec, dir);
 	// printf("dot: %f\n", result);
 	// t_cor	ori = {5, -2, 0};
